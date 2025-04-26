22a4931449177567013609cfcbb27945ad2f2372
Muhammad Muhajir <muhajirframe@gmail.com>
2025-04-26 17:09:27 +0700

    .

--- apps/web/app/components/data-explorer/types.ts
+++ apps/web/app/components/data-explorer/types.ts
@@ -18,3 +18,31 @@ export interface GraphData {
   nodes: GraphNode[];
   links: GraphLink[];
 }
+
+// Define types for schema data
+export interface SchemaProperty {
+  name: string;
+  type: string;
+  isPrimaryKey?: boolean;
+}
+
+export interface NodeTable {
+  name: string;
+  properties: SchemaProperty[];
+}
+
+export interface RelationshipConnectivity {
+  src: string;
+  dst: string;
+}
+
+export interface RelationshipTable {
+  name: string;
+  properties: SchemaProperty[];
+  connectivity: RelationshipConnectivity[];
+}
+
+export interface SchemaData {
+  nodeTables: NodeTable[];
+  relTables: RelationshipTable[];
+}
